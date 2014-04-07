970e9cbd218b724e0e44ce476e8d9ec2008aefd8
tsubakimoto_s <tkmbmbr@gmail.com>
2014-04-07 23:11:53 +0900

    コンソールプロジェクトでの検索処理を作成

--- mlb_video_player_console/Program.cs
+++ mlb_video_player_console/Program.cs
@@ -1,15 +1,75 @@
-﻿using System;
+﻿using Google.Apis.YouTube.v3;
+using google_api_credential;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace mlb_video_player_console
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine("YouTube Data API: Search");
+            Console.WriteLine("========================");
+
+            try
+            {
+                new Program().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task Run()
+        {
+            var youtubeService = new YouTubeService(Credential.GetInitializer());
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "mlb cg"; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+            searchListRequest.Order = SearchResource.ListRequest.OrderEnum.Date;
+            searchListRequest.PublishedAfter = DateTime.Today;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            var videos = new List<string>();
+            var channels = new List<string>();
+            var playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
         }
     }
 }
