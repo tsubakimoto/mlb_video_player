1172c461b410259b725f4f849ee2c7f8609b9ae7
tsubakimoto_s <tkmbmbr@gmail.com>
2014-04-07 22:58:26 +0900

    コンソールプロジェクトを追加

---
+++ mlb_video_player_console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mlb_video_player_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
