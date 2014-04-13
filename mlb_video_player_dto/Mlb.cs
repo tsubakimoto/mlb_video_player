db7e4cabbd4ce0ea23f09168f5475e588567afb4
tsubakimoto_s <tkmbmbr@gmail.com>
2014-04-13 12:55:54 +0900

    MLBクラスの追加

---
+++ mlb_video_player_dto/Mlb.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mlb_video_player_dto
+{
+    public class Mlb
+    {
+        #region プロパティ
+
+        public List<League> Leagues { get; set; }
+
+        #endregion
+
+        #region コンストラクタ
+
+        public Mlb()
+        {
+
+        }
+
+        #endregion
+    }
+
+    public class League
+    {
+        #region プロパティ
+
+        public string Name { get; set; }
+
+        public LeagueType Type { get; set; }
+
+        public List<Team> Teams { get; set; }
+
+        #endregion
+
+        #region 列挙型
+
+        public enum LeagueType
+        {
+            American,
+            National
+        }
+
+        #endregion
+
+        #region コンストラクタ
+
+        public League()
+        {
+
+        }
+
+        #endregion
+    }
+
+    public class Team
+    {
+        #region プロパティ
+
+        public string Name { get; set; }
+
+        public TeamDivision Division { get; set; }
+
+        #endregion
+
+        #region 列挙型
+
+        public enum TeamDivision
+        {
+            East,
+            Central,
+            West
+        }
+
+        #endregion
+
+        #region コンストラクタ
+
+        public Team()
+        {
+
+        }
+
+        #endregion
+    }
+}
